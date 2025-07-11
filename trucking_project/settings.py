901f36151f883cb2a2a7c163f769e9611ab923cf
ahmedgaber19 <ahmed.gaber428@yahoo.com>
2025-07-11 03:57:11 +0300

    Simplify Vercel deployment: remove static files build, use API-only configuration

--- trucking_project/settings.py
+++ trucking_project/settings.py
@@ -97,7 +97,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# Simplified for API-only deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
