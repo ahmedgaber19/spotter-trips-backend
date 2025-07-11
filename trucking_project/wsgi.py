e8e0ab1e2dfbdd6667a82ecf5b841cfb4bfb46aa
ahmedgaber19 <ahmed.gaber428@yahoo.com>
2025-07-11 04:00:15 +0300

    Fix WSGI configuration: add handler and app variables for Vercel compatibility

--- trucking_project/wsgi.py
+++ trucking_project/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trucking_project.settings')
 
 application = get_wsgi_application()
+
+# Vercel expects either 'app' or 'handler'
+app = application
+handler = application
